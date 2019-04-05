944726c0e3b2c273a76832b210b9509a595bec0e
Pankrat <lha@ableton.com>
2019-04-05 18:49:56 +0200

    Add classifiers

--- setup.py
+++ setup.py
@@ -15,4 +15,13 @@ setup(
     install_requires=["pika >= 1.0.0"],
     tests_require=["pytest >= 3.6.0", "mock"],
     zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
