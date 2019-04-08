e0883db2bfe5b32bdd73f44a9288fa8483bca08c
Pankrat <lha@ableton.com>
2019-04-08 11:02:29 +0200

    Add project description for PyPI

--- setup.py
+++ setup.py
@@ -2,11 +2,19 @@ import versioneer
 
 from setuptools import setup, find_packages
 
+# Add README as description
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='domain-event-broker',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     description='Send and receive domain events via RabbitMQ',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Ableton AG',
     author_email='webteam@ableton.com',
     url='https://github.com/AbletonAG/domain-event-broker',
